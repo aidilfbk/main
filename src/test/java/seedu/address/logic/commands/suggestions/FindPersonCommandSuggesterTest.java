2a604250db95ae95d44aaa0282d0c1f5aff5e9aa
aidilfbk <e0303295@u.nus.edu>
2019-11-10 18:10:20 +0800

    Delete suggester for already-removed `findperson` command

--- src/test/java/seedu/address/logic/commands/suggestions/FindPersonCommandSuggesterTest.java
+++
@@ -1,8 +0,0 @@
-package seedu.address.logic.commands.suggestions;
-
-class FindPersonCommandSuggesterTest extends SuggesterImplTester {
-
-    protected FindPersonCommandSuggesterTest() throws ReflectiveOperationException {
-        super(FindPersonCommandSuggester.class);
-    }
-}
